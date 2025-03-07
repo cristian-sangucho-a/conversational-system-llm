08ee587e3e84adf563d898c758786b13a56592b8
Cristtsirc <cristian.sangucho@epn.edu.ec>
2025-03-07 13:44:00 -0500

    fix(routers): add dependency session to chat router

--- app/routers/chat_router.py
+++ app/routers/chat_router.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Depends, Form, UploadFile, File
-from services.ask_via_audio import AskViaAudio
+from app.services.ask_via_audio_service import AskViaAudioService
+from core.dependencies import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+
 chat_router = APIRouter()
 
 @chat_router.post(
@@ -8,8 +11,9 @@ async def ask_via_audio(
     user_id: int = Form(...), 
     conversation_id: int = Form(...), 
     file: UploadFile = File(...),
+    db: AsyncSession = Depends(get_db)
 ):
-    ask_via_audio = AskViaAudio()
+    ask_via_audio = AskViaAudioService(db)
     return await ask_via_audio.model_response_via_audio(file, user_id, conversation_id)
      
     
